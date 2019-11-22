b2abbdb6b12409fd05f39c9820b490fb025b4f5f
Yuta Takahashi <penityai@gmail.com>
2019-11-23 03:51:00 +0900

    add LocalStorage.js

---
+++ src/lib/LocalStorage.js
@@ -0,0 +1,18 @@
+export const LocalStorage = {
+  /**
+   * ローカルストレージからデータを呼び出す
+   * @param STORAGE_KEY
+   * @returns {any}
+   */
+  fetch(STORAGE_KEY) {
+    return JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]");
+  },
+  /**
+   * ローカルストレージにデータを格納する
+   * @param STORAGE_KEY
+   * @param DATA
+   */
+  set(STORAGE_KEY, DATA) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(DATA));
+  }
+};
