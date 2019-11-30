75f69d48c5ba261ac740a19389564787cd1dc2e7
Yuta Takahashi <penityai@gmail.com>
2019-11-30 16:11:12 +0900

    Added InitialScreen.vue for the initial screen

---
+++ src/components/InitialScreen.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="initial-screen">初期画面</div>
+</template>
+
+<script>
+export default {
+  name: "InitialScreen"
+};
+</script>
+
+<style scoped lang="scss">
+.initial-screen {
+  font-size: 20px;
+}
+</style>
