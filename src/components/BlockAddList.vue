5a0d8e3654c040305c6a609577d0719e803b0f73
Yuta Takahashi <penityai@gmail.com>
2019-12-02 02:21:26 +0900

    Create a block component consisting of a button and a form to add a list

---
+++ src/components/BlockAddList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="block-add-list">
+    <button @click="addNewList">リストを追加する</button>
+    <form class="block-add-list--form">
+      <label>
+        <input type="text" />
+      </label>
+      <div>
+        <button>リストを追加</button>
+        <button>
+          <font-awesome-icon :icon="['fas', 'times-circle']" />
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ButtonAddList",
+  methods: {
+    addNewList() {}
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.button--add-list {}
+</style>
