c44bd2156d069d8b4a006ab3518212fbffad0be4
Yuta Takahashi <penityai@gmail.com>
2019-12-02 02:19:45 +0900

    Create a button component to add

---
+++ src/components/ButtonAdd.vue
@@ -0,0 +1,33 @@
+<template>
+  <button class="button-add">
+    <font-awesome-icon :icon="['fas', 'plus-circle']" />作成する
+  </button>
+</template>
+
+<script>
+export default {
+  name: "ButtonAdd"
+};
+</script>
+
+<style scoped lang="scss">
+.button-add {
+  margin-top: 10px;
+  width: 100px;
+  border-radius: 2.5px;
+  height: 30px;
+  font-size: 14px;
+  line-height: 30px;
+  background-color: $COLOR_GREEN;
+  color: #fff;
+
+  &.no-input {
+    cursor: not-allowed;
+    background-color: lighten($COLOR_GREEN, 20%);
+  }
+
+  svg {
+    margin-right: 5px;
+  }
+}
+</style>
